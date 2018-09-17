ea9f15949a8ef5e4c58aac4ab864ecd479e7cbd2
Toncho Lozev <Toncho.Lozev@endava.com>
2018-09-17 17:31:49 +0300

    exercise

--- 05.Exercise-Persistance-Files-and-MongoDB/MongoPlayground/handlers/addImageHandler.js
+++ 05.Exercise-Persistance-Files-and-MongoDB/MongoPlayground/handlers/addImageHandler.js
@@ -1,6 +1,7 @@
 const ObjectId = require('mongoose').Types.ObjectId;
 const formidable = require('formidable');
 const Image = require('mongoose').model('Image');
+const Tag = require('mongoose').model('Tag');
 
 module.exports = (req, res) => {
     if (req.pathname === '/addImage' && req.method === 'POST') {
@@ -23,26 +24,56 @@ function addImage(req, res) {
         }
         res.writeHead(200, {'content-type': 'text/plain'});
 
-        let tags =  fields.tagsID.split(',').slice(0, -1);
+        let tags = fields.tagsID.split(',').slice(0, -1);
         tags = tags.filter((e, i) => {
             return tags.indexOf(e) === i;
         }).map(ObjectId);
+
 
         Image.create({
             url: fields.imageUrl,
             description: fields.description,
             tags: tags
         }).then(image => {
+            for (let tag of tags) {
+                Tag.findById(tag, function (err, data) {
+                    const currentImages = data.images;
+                    currentImages.push(image._id);
+
+                    data.images = currentImages;
+                    data.save().then((err, data)=> {
+                        if(err){
+                            throw err;
+                        }
+                    })
+                });
+            }
+
             res.writeHead(302, {
                 location: '/'
             });
             res.end();
         }).catch(err => {
-            cres.writeHead(500, {
+            res.writeHead(500, {
                 'Content-Type': 'text/plain'
             });
-            res.write('500 Server Error');
+            res.write('500 Server Erro r');
             res.end();
         });
+    });
+}
+
+function deleteImg(req, res) {
+    Image.deleteOne({_id: req.pathquery.id}).then(() => {
+        res.writeHead(302, {
+            location: '/'
+        });
+        res.end();
+    }).catch(err => {
+        res.writeHead(500, {
+            'Content-Type': 'text/plain'
+        });
+        res.write('500 Server Erro r');
+        res.end();
     });
 }
--- 05.Exercise-Persistance-Files-and-MongoDB/MongoPlayground/handlers/searchHandler.js
+++ 05.Exercise-Persistance-Files-and-MongoDB/MongoPlayground/handlers/searchHandler.js
@@ -1,8 +1,68 @@
+const Image = require('mongoose').model('Image');
+const Tag = require('mongoose').model('Tag');
+const fs = require('fs');
 
 module.exports = (req, res) => {
-  if (req.pathname === '/search') {
-   
-  } else {
-    return true
-  }
+    if (req.pathname === '/search') {
+        fs.readFile('./views/results.html', 'utf8', (err, html) => {
+            if (err) {
+                throw err;
+            }
+            const params = {};
+            if (req.pathquery.tagName) {
+                const tags = req.pathquery.tagName.split(',').filter(e => e.length > 0);
+                if (tags.length > 0) {
+                    Tag.find({name: {$in: tags}}).then(tagsId => {
+                        findCats(req, res, tagsId, html);
+                    })
+                }
+            }
+            if (req.pathquery.afterDate && req.pathquery.beforeDate) {
+                console.log('both')
+                return;
+            }
+            if (req.pathquery.afterDate) {
+                console.log('after');
+            }
+            if (req.pathquery.beforeDate) {
+                console.log('before')
+            }
+            if (req.pathquery.Limit) {
+                console.log('limit')
+            }
+        });
+    } else {
+        return true
+    }
+};
+
+async function findCats(req, res, imagesId, html) {
+    const images = [];
+    let imageHtml = '';
+    for (let tag of imagesId) {
+        for (let image of tag.images) {
+            await Image.findById(image, function (err, data) {
+                images.push(data);
+            })
+        }
+    }
+    if (images.length > 0) {
+        for (let image of images) {
+            imageHtml += `<fieldset id ="${image._id}">
+                <img src="${image.url}"/>
+                <p>${image.description}<p/>
+                <button onclick='location.href="/delete?id=${image._id}"' class='deleteBtn'>Delete</button>
+             </fieldset>`;
+        }
+    } else {
+        imageHtml += `<div>No images found!</div>`
+    }
+
+    html = html.replace("<div class='replaceMe'></div>", imageHtml);
+
+    res.writeHead(200, {
+        'Content-Type': 'text/html'
+    });
+    res.write(html);
+    res.end();
 }
